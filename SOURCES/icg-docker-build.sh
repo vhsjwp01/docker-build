b6896b925d762bb4e59e9149e89131a3e5c45c7f
Plummer, Jason <Jason.Plummer@ingramcontent.com>
2015-08-14 15:30:21 -0500

    Fixed github uri parsing - must be in HTTP form

--- SOURCES/icg-docker-build.sh
+++ SOURCES/icg-docker-build.sh
@@ -457,7 +457,7 @@ if [ ${exit_code} -eq ${SUCCESS} ]; then
     # STASH_BASE_URI, stash_project, and docker_namespace
     # git@github.com:vitalsource/vst-client.git
     if [ "${github_project}" != "" ]; then
-        stash_base_uri=`echo "${github_project}" | ${my_awk} -F':' '{print $1}'`
+        stash_base_uri=`echo "${github_project}" | ${my_awk} -F'/' '{print $1}'`
         docker_namespace=`echo "${github_project}" | ${my_awk} -F':' '{print $2}' | awk -F'/' '{print $1}'`
         stash_project=`echo "${github_project}" | ${my_awk} -F'/' '{print $NF}'`
         STASH_BASE_URI="${stash_base_uri}"
